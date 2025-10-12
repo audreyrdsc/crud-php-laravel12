22fcb27dfcdca71bebfb7a96c8478a55c657a88c
audreyrdsc <audreyrdsc@gmail.com>
2025-10-12 13:42:46 -0300

    Criação da Model Rede.php e da tabela por migration

---
+++ app/Models/Rede.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rede extends Model
+{
+    protected $table = 'redes';
+    protected $fillable = [
+        'nome',
+        'link'
+    ];
+}
